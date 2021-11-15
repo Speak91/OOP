ea32d5625f64de0ad5607d1c178c89c095609cb5
Speak91 <i-valentin@mail.ru>
2021-11-15 19:14:19 +0900

    Выполнена 2 часть 2-го ДЗ
    Изменить класс счет в банке из упражнения таким образом, чтобы номер счета генерировался сам и был уникальным. Для этого надо создать в классе статическую переменную и метод, который увеличивает значение этого переменной.

--- HW2/HomeWork2/HomeWork2/Program.cs
+++ HW2/HomeWork2/HomeWork2/Program.cs
@@ -29,20 +29,32 @@ namespace HomeWork2
     class Account
     {
         private int _balance;
-        private int _accountNumber;
+        private string _accountNumber;
         private string _accountType;
-       
-        public void MakeADeposit(int accountNumber, int balance, bankAccountType AccountType)
+        private static string _generateAccountNumber;
+
+        public Account()
+        {
+            GeneateAccountNumber();
+        }
+
+        public void MakeADeposit(int balance, bankAccountType AccountType)
         {
              
             _balance = balance;
-            _accountNumber = accountNumber;
+            _accountNumber = _generateAccountNumber;
             _accountType = AccountType.ToString();
         }
 
         public void Display ()
         {
             Console.WriteLine($"Номер счета: {_accountNumber}, Баланс счета: {_balance} рублей, Тип счета: {_accountType}");
+        }
+
+        private static void GeneateAccountNumber()
+        {
+            Random random = new Random();
+            _generateAccountNumber = Guid.NewGuid().ToString();
         }
     }
     class Program
@@ -51,7 +63,11 @@ namespace HomeWork2
         static void Main(string[] args)
         {
             Account account = new Account();
-            account.MakeADeposit(100, 124124124, bankAccountType.Сберегательный);
+            account.MakeADeposit(100, bankAccountType.Сберегательный);
+            account.Display();
+
+            Account account2 = new Account();
+            account.MakeADeposit(100, bankAccountType.Инвестиционный);
             account.Display();
         }
     }
