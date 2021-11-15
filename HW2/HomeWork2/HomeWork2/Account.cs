fb6a6c01b8626dfc8e1f189f07e5dcbfa5ed413a
Speak91 <i-valentin@mail.ru>
2021-11-15 20:59:48 +0900

    Выполнено 4-5 часть 2 - го ДЗ
    В классе все методы для заполнения и получения значений полей заменить на свойства. Написать тестовый пример.
    
    (*)Добавить в класс счет в банке два метода: снять со счета и положить на счет. Метод снять со счета проверяет, возможно ли снять запрашиваемую сумму, и в случае положительного результата изменяет баланс.

---
+++ HW2/HomeWork2/HomeWork2/Account.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    /// <summary>
+    /// Класс банковский счет
+    /// </summary>
+    class Account
+    {
+        private int _balance;
+        private string _accountNumber;
+        private string _accountType;
+        private static string _generateAccountNumber;
+
+        public int Balance { get { return _balance; } set { _balance = value; } }
+        public string AccountNumber { get {return _accountNumber; } private set { _accountNumber = value; } }
+        public string AccountType { get { return _accountType; } set { _accountType = value; } }
+        
+        public Account(bankAccountType bankAccountType) : this(0)
+        {
+            this.AccountType = bankAccountType.ToString();
+            this.AccountNumber = GenerateAccountNumber();
+        }
+
+        public Account(int balance) : this(balance, bankAccountType.Сберегательный)
+        {
+            this.AccountNumber = GenerateAccountNumber();
+        }
+
+        public Account(int balance, bankAccountType AccountType)
+        {
+           this.Balance = balance;
+           this.AccountType = AccountType.ToString();
+           this.AccountNumber = GenerateAccountNumber();
+        }
+
+        /// <summary>
+        /// Снятие со счета
+        /// </summary>
+        /// <param name="sum">сумма</param>
+        public void WithDraw(int sum)
+        {
+            if (_balance > sum)
+            {
+                _balance -= sum;
+                Console.WriteLine($"Сумма в размере {sum} руб снята со счета ваш баланс составляет {Balance} руб");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств для снятия со счета");
+            }
+        }
+
+        /// <summary>
+        /// Пополнение счета
+        /// </summary>
+        /// <param name="sum">Сумма</param>
+        public void DepositACheck(int sum)
+        {
+            _balance += sum;
+            Console.WriteLine($"Ваш счет пополнен в размере {sum} руб, ваш баланс составляет {Balance} руб");
+        }
+
+        /// <summary>
+        /// Показать информацию о счете
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine($"Номер счета: {AccountNumber}, Баланс счета: {Balance} руб, Тип счета: {AccountType}");
+        }
+
+        private static string GenerateAccountNumber()
+        {
+            return _generateAccountNumber = Guid.NewGuid().ToString();
+        }
+    }
+}
--- HW2/HomeWork2/HomeWork2/Program.cs
+++ HW2/HomeWork2/HomeWork2/Program.cs
@@ -2,65 +2,6 @@
 
 namespace HomeWork2
 {
-    /// <summary>
-    /// Типы банковских счетов 
-    /// </summary>
-    public enum bankAccountType
-    {
-        /// <summary>
-        /// Это сберегательный счет для длительного хранения денег
-        /// </summary>
-        Сберегательный,
-        
-        /// <summary>
-        /// Это инвестиционный счет
-        /// </summary>
-        Инвестиционный,
-
-        /// <summary>
-        /// Чековый счет
-        /// </summary>
-        Чековый
-    }
-
-    /// <summary>
-    /// Класс банковский счет
-    /// </summary>
-    class Account
-    {
-        private int _balance;
-        private string _accountNumber;
-        private string _accountType;
-        private static string _generateAccountNumber;
-
-        public Account(bankAccountType bankAccountType) : this(0)
-        {
-            _accountType = bankAccountType.ToString();
-            _accountNumber = GenerateAccountNumber();
-        }
-
-        public Account(int balance) : this(balance, bankAccountType.Сберегательный)
-        {
-            _accountNumber = GenerateAccountNumber();
-        }
-
-        public Account(int balance, bankAccountType AccountType)
-        {
-            _balance = balance;
-            _accountType = AccountType.ToString();
-            _accountNumber = GenerateAccountNumber();
-        }
-
-        public void Display()
-        {
-            Console.WriteLine($"Номер счета: {_accountNumber}, Баланс счета: {_balance} рублей, Тип счета: {_accountType}");
-        }
-
-        private static string GenerateAccountNumber()
-        {
-            return _generateAccountNumber = Guid.NewGuid().ToString();
-        }
-    }
     class Program
     {
 
@@ -68,15 +9,23 @@ namespace HomeWork2
         {
             Account account = new Account(10, bankAccountType.Чековый);
             account.Display();
+            account.WithDraw(5);
+            account.WithDraw(15);
+            Console.WriteLine();
 
             Account account2 = new Account(50);
             account2.Display();
+            account2.DepositACheck(100);
+            Console.WriteLine();
 
             Account account3 = new Account(bankAccountType.Инвестиционный);
             account3.Display();
-
+            account3.WithDraw(15);
+            Console.WriteLine();
+            
             Account account4 = new Account(bankAccountType.Чековый);
             account4.Display();
+            account4.DepositACheck(150);
         }
     }
 }
