914752e33c548c7ddd5cd59d1706e321720f1fd7
Speak91 <i-valentin@mail.ru>
2021-11-15 19:04:30 +0900

    Выполнена 1 часть 2-го ДЗ
    Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип). Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать

---
+++ HW2/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    /// <summary>
+    /// Типы банковских счетов 
+    /// </summary>
+    public enum bankAccountType
+    {
+        /// <summary>
+        /// Это сберегательный счет для длительного хранения денег
+        /// </summary>
+        Сберегательный,
+        
+        /// <summary>
+        /// Это инвестиционный счет
+        /// </summary>
+        Инвестиционный,
+
+        /// <summary>
+        /// Чековый счет
+        /// </summary>
+        Чековый
+    }
+
+    /// <summary>
+    /// Класс банковский счет
+    /// </summary>
+    class Account
+    {
+        private int _balance;
+        private int _accountNumber;
+        private string _accountType;
+       
+        public void MakeADeposit(int accountNumber, int balance, bankAccountType AccountType)
+        {
+             
+            _balance = balance;
+            _accountNumber = accountNumber;
+            _accountType = AccountType.ToString();
+        }
+
+        public void Display ()
+        {
+            Console.WriteLine($"Номер счета: {_accountNumber}, Баланс счета: {_balance} рублей, Тип счета: {_accountType}");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.MakeADeposit(100, 124124124, bankAccountType.Сберегательный);
+            account.Display();
+        }
+    }
+}
