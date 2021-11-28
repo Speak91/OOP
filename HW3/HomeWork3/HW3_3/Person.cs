8dc3310dc020f46a2c354f0d914c711f467297a3
Speak91 <i-valentin@mail.ru>
2021-11-28 21:01:54 +0900

    Выполнено 3 задание 3 урока

---
+++ HW3/HomeWork3/HW3_3/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace HW3_3
+{
+    /// <summary>
+    /// Класс персона для записи имени и Email Адреса из файла
+    /// </summary>
+    class Person
+    {
+        private string _name;
+        private string _email;
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
