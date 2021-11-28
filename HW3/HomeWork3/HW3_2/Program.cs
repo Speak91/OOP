beeadd6cef93a1f3ab259fac5db3f5d30666e906
Speak91 <i-valentin@mail.ru>
2021-11-28 19:17:52 +0900

    Выполнено 2 задание 3 урока

---
+++ HW3/HomeWork3/HW3_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reverseString = FlipTheLine("ABCDEFGH");
+            Console.WriteLine(reverseString);
+        }
+
+        static string FlipTheLine(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            string reverseString = new string(array);
+            return reverseString;
+        }
+    }
+}
