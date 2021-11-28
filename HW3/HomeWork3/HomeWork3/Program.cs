cb0c0e8bf11221891d9a65afc6f6c661257c8686
Speak91 <i-valentin@mail.ru>
2021-11-28 18:55:36 +0900

    Выполнено 1 задание 3 урока

---
+++ HW3/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Account account = new Account(10, bankAccountType.Чековый);
+            account.Display();
+            account.WithDraw(5);
+            account.WithDraw(15);
+            Console.WriteLine();
+
+            Account account2 = new Account(50);
+            account2.Display();
+            account2.DepositACheck(1000);
+            Console.WriteLine();
+
+            Account account3 = new Account(bankAccountType.Инвестиционный);
+            account3.Display();
+            account3.DepositACheck(500);
+            account3.TransferFundsToCurrentAccount(account2, 500); //Снимаем деньги со счета
+            account3.Display();
+            Console.WriteLine();
+            account2.Display();
+        }
+    }
+}
